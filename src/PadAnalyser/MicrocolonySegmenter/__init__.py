4aca6832f18e39c73d486bfcb0168a78ad73b774
Morten Kals <morten@kals.no>
2023-09-06 16:16:48 +0100

    cleanup in variable generation

--- src/PadAnalyser/MicrocolonySegmenter/__init__.py
+++ src/PadAnalyser/MicrocolonySegmenter/__init__.py
@@ -1,5 +1,5 @@
 SEGMENTATION_VERSION = 'S1.23'
-DATAFRAME_VERSION = 'D1.13'
+DATAFRAME_VERSION = 'D1.14'
 PLOT_VERSION = 'P1.21'
 MODEL_VERSION = 'M1.03'
 
@@ -21,67 +21,30 @@ from .DInfo import DInfo
 from . import MKAnalysisUtils
 
 
-def segment_frame_set(frame_set: FrameSet, output_config: OutputConfig) -> pd.DataFrame:
-    """Segment colonies and single cells of frame set.
-
-    Produces two intermediate files with segmentation output and dataframe output, that can both be used at cache to speed up expensive calculations.
-
-    Args:
-        frame_set (FrameSet): _description_
-        output_config (OutputConfig): _description_
-
-    Returns:
-        pd.DataFrame: _description_
-    """
+def segment_frame_set_to_dict(frame_set: FrameSet, output_config: OutputConfig) -> dict:
     
     label = frame_set.label
     clear_dirs = output_config.clear_dirs
-    
-    # Logging dir setup
-    loggin_dir = os.path.dirname(output_config.logging_file)
-    MKUtils.generate_directory(loggin_dir)
-    MKUtils.configure_logger(filepath=output_config.logging_file, label=f'subprocess', file_mode='a', stream_level='WARN') # reconfigure in each multiprocessing pool
-
-    # Output dir setup
-    frameset_output_dir = output_config.output_dir
-    frameset_segmentation_dir = os.path.join(frameset_output_dir, 'segmentation')
-    frameset_dataframe_dir = os.path.join(frameset_output_dir, 'dataframe')
-    MKUtils.generate_directory(frameset_segmentation_dir, clear=clear_dirs) # make directories if they do not exist
-    MKUtils.generate_directory(frameset_dataframe_dir, clear=clear_dirs)
-
-    # Debug dir setup
+    output_dir = output_config.output_dir
     debug_dir = output_config.debug_dir
-    image_dir = os.path.join(debug_dir, 'img')
-    video_dir = os.path.join(debug_dir, 'mov')
-    MKUtils.generate_directory(video_dir, clear=clear_dirs)
-    MKUtils.generate_directory(image_dir, clear=clear_dirs)
+    mask_dir = output_config.mask_dir
 
-    frameset_segmentation_file = os.path.join(frameset_segmentation_dir, f'{label}_{SEGMENTATION_VERSION}.json')
-    frameset_dataframe_file = os.path.join(frameset_dataframe_dir, f'{label}_{DATAFRAME_VERSION}.json') # file where data from this experiment is stored
-
-    # try to recover dataframe from cache file
-    if output_config.cache_dataframe:
-        try:
-            return pd.read_json(path_or_buf=frameset_dataframe_file)
-        except (ValueError, FileNotFoundError):
-            pass
-        except Exception:
-            logging.exception(f'Could not load dataframe.')
-
+    image_dir = MKUtils.join_and_make_path(debug_dir, 'img', clear=clear_dirs)
+    video_dir = MKUtils.join_and_make_path(debug_dir, 'mov', clear=clear_dirs)
+    segmentation_file = MKUtils.join_and_make_path(output_dir, 'segmentation', f'{label}_{SEGMENTATION_VERSION}.json')
 
     # try to recover segmentation information from file
     if output_config.cache_segmentation:
         try:
-            with open(frameset_segmentation_file, 'r') as f: file_contents = f.read()
+            with open(segmentation_file, 'r') as f: file_contents = f.read()
             logging.info('Loaded log file.')
             return json.loads(file_contents)
         except FileNotFoundError:
             pass
         except json.decoder.JSONDecodeError as e:
-            logging.debug(f'Could not decode json from file {frameset_segmentation_file}, {e}')
+            logging.debug(f'Could not decode json from file {segmentation_file}, {e}')
         except Exception as e:
             logging.exception('Could not load segmentation data from cache.')
-
 
     dinfo = DInfo(
         label=label,
@@ -97,17 +60,45 @@ def segment_frame_set(frame_set: FrameSet, output_config: OutputConfig) -> pd.Da
     logging.info(f'Analysing {label}')
     data = MKTimeseriesAnalyzer.analyze_time_seriess(
         frame_set=frame_set,
-        mask_folder=output_config.mask_dir, 
+        mask_folder=mask_dir, 
         label=label, 
         dinfo=dinfo,
     )
     
     MKAnalysisUtils.write_dict_to_json(
         data=data,
-        filename=frameset_segmentation_file,
+        filename=segmentation_file,
     )
     logging.info(f'Done analyzing {label}')
 
+    return data
+
+
+def segment_frame_set(frame_set: FrameSet, output_config: OutputConfig) -> pd.DataFrame:
+    """Segment colonies and single cells of frame set.
+
+    Produces two intermediate files with segmentation output and dataframe output, that can both be used at cache to speed up expensive calculations.
+
+    Args:
+        frame_set (FrameSet): _description_
+        output_config (OutputConfig): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    
+    label = frame_set.label
+    
+    # Logging dir setup
+    loggin_dir = os.path.dirname(output_config.logging_file)
+    MKUtils.generate_directory(loggin_dir)
+    MKUtils.configure_logger(filepath=output_config.logging_file, label=f'subprocess', file_mode='a', stream_level='WARN') # reconfigure in each multiprocessing pool
+        
+    data = segment_frame_set_to_dict(
+        frame_set=frame_set, 
+        output_config=output_config, 
+    )
+    
     # convert to dataframe and add growth rate etc.
     logging.info(f'Making dataframe for {label}')
     df = DataProcessor.dataframe_from_data_series(
@@ -115,18 +106,17 @@ def segment_frame_set(frame_set: FrameSet, output_config: OutputConfig) -> pd.Da
         label=label,
         metadata=frame_set.metadata
     )
-    df.to_json(frameset_dataframe_file)
     logging.info(f'Finished making dataframe for {label}')
 
     return df
     
 
 def dataframe_filepath(directory: str) -> str:
-    return os.path.join(directory, f'dataframe_{DATAFRAME_VERSION}.parquet.gzip')
+    return os.path.join(directory, f'dataframe_{DATAFRAME_VERSION}.json')
 
 def load_dataframe(dataframe_file: str) -> Optional[pd.DataFrame]:
     try:
-        return pd.read_parquet(path=dataframe_file)
+        return pd.read_json(path_or_buf=dataframe_file)
     except (ValueError, FileNotFoundError):
         pass
     except Exception:
@@ -168,14 +158,6 @@ def segment_frame_sets(frame_sets: List[FrameSet], output_config: OutputConfig):
 
     df = pd.concat(dataframes, axis=0)
     df.reset_index(inplace=True)
-    df.to_parquet(dataframe_file) # save to file 
-
-
-
-    # set round_time based on smallest time with given round_index
-    # find time series that starts with smallest value (i.e. is from first imaged location) for round_times
-    # time_series = [s['time'] for s in data_series]
-    # index_of_first_time_series = np.argmin([s[0] for s in time_series])
-    # round_times = np.array(time_series[index_of_first_time_series])
+    df.to_json(dataframe_file) # save to file 
     
     return df
